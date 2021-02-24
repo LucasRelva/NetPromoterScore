8c2051a442c55d43b286641e62a01cc015ccd7b6
Lucas Relva <lucas-1880@outlook.com>
2021-02-23 21:47:48 -0300

    Minor changes, db config, first method

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
+import 'reflect-metadata'
 import express from 'express'
+import './database' // por padrão o arquivo index é importado, /index.ts não é necessário informar
+import { router } from './routes'
 
 const app = express()
+
+app.use(express.json())
+app.use(router)
 
 app.listen(8000, () => {
     console.log('Servidor aberto na porta: 8000')
